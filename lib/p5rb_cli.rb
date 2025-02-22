aa7f5ee95a0fbc786903c7b0cc766096caf7a9dc
ongaeshi <ongaeshi0621@gmail.com>
2025-02-23 03:45:05 +0900

    feat: Add P5rbCli::Cli

--- lib/p5rb_cli.rb
+++ lib/p5rb_cli.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 require_relative "p5rb_cli/version"
+require "thor"
+require "fileutils"
 
 module P5rbCli
-  class Error < StandardError; end
-  # Your code goes here...
+  class Cli < Thor
+    def self.exit_on_failure?
+      true
+    end
+  
+    desc "new NAME", "Create a new p5rb script"
+    def new(name)
+      if FileTest.exist?(name)
+        raise Thor::Error, "ERROR: '#{File.absolute_path(name)}' already exists"
+      end
+  
+      p name
+    end
+  end
 end
